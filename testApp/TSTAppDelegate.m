cd30aa34cc969cf72fbe9477937443c2c6d84d10
Oleksiy Ivanov <oleksiy@effectivedeveloper.com>
2014-01-09 15:46:58 +0000

    Added basic reproducible case of crash when dispatch_get_current_queue is used. This crashing on both OSX 10.8 and OSX 10.9 .

--- testApp/TSTAppDelegate.m
+++ testApp/TSTAppDelegate.m
@@ -13,6 +13,15 @@
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
     // Insert code here to initialize your application
+    
+    dispatch_queue_t queue = dispatch_queue_create("test queue", DISPATCH_QUEUE_SERIAL);
+    
+    double delayInSeconds = 2.0;
+    dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, (int64_t)(delayInSeconds * NSEC_PER_SEC));
+    dispatch_after(popTime, queue, ^(void){
+        dispatch_queue_t currentQueue = dispatch_get_current_queue();
+        NSLog(@"Current queue label %s", dispatch_queue_get_label(currentQueue));
+    });
 }
 
 @end
